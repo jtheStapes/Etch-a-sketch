23eaf14cf26c249a3c444d3965fe688c92e16b66
Josh <jrstaples19@gmail.com>
2022-08-09 14:41:41 -0500

    Add eraser button

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const grid = document.createElement('div');
 const body = document.createElement('div');
 const sidebar = document.createElement('div'); //Create sidebar for erasing, changing colors, clearing, etc
 const clear = document.createElement('button');
+const eraser = document.createElement('button');
 const newGrid = document.createElement('button');
 const header = document.querySelector('h1');
 
@@ -14,10 +15,13 @@ clear.classList.add('clear');
 clear.textContent = 'Clear';
 newGrid.classList.add('newGrid');
 newGrid.textContent = 'New';
+eraser.classList.add('eraser');
+eraser.textContent = 'Eraser';
 
-initialGrid();
-sidebar.appendChild(clear);
+makeGrid();
 sidebar.appendChild(newGrid);
+sidebar.appendChild(eraser);
+sidebar.appendChild(clear);
 body.appendChild(sidebar);
 body.appendChild(grid);
 container.appendChild(body);
@@ -26,36 +30,49 @@ clear.addEventListener('click', () => { //Clears the grid
     clearGrid();
 })
 
-newGrid.addEventListener('click', () => {
-    initialGrid();
+newGrid.addEventListener('click', () => { //Clears grid then makes a new one based on size inputted by user
+    makeGrid();
     clearGrid();
     colorGrid();
+})
+
+eraser.addEventListener('click', () => {
+    eraseGridItems();
 })
 
 const gridItems = document.querySelectorAll('.box');
 for (let gridItem of gridItems) {
     gridItem.addEventListener('mouseover', function () { //Colors the grid items on mouseover
-        gridItem.style.background = 'purple';
+        gridItem.style.background = 'black';
     }
 )}
 
-function colorGrid () {
+function colorGrid () { //Colors each div when mouse passes over
     const gridItems = document.querySelectorAll('.box');
     for (let gridItem of gridItems) {
         gridItem.addEventListener('mouseover', function () { //Colors the grid items on mouseover
-            gridItem.style.background = 'purple';
+            gridItem.style.background = 'black';
         }
     )}
 }
 
-function clearGrid() {
+function eraseGridItems () {
+    const gridItems = document.querySelectorAll('.box');
+    for (let gridItem of gridItems) {
+        gridItem.addEventListener('mouseover', function () { //Colors the grid items on mouseover
+            gridItem.style.background = 'white';
+        }
+    )}
+}
+
+function clearGrid() { //Clears the current grid
     const gridItems = document.querySelectorAll('.box');
     gridItems.forEach(gridItem => {
         gridItem.style.background = 'white';
     })
 }
 
-function initialGrid() { //Creates the initial grid with the size based on user input
+function makeGrid() { //Creates a grid with the size based on user input
     numBoxes = parseInt(prompt ("Enter grid width in # of boxes (cannot exceed 64)"));
     while (numBoxes > 64) {
         numBoxes = parseInt(prompt ("Too large a number. Enter a grid width of 64 or lower"));
