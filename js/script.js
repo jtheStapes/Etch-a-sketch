6c105b8df29653eaf9148ad17ef44c0c9041af47
Josh <jrstaples19@gmail.com>
2022-08-09 14:24:28 -0500

    Create button to make new grid and make functions for them

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const grid = document.createElement('div');
 const body = document.createElement('div');
 const sidebar = document.createElement('div'); //Create sidebar for erasing, changing colors, clearing, etc
 const clear = document.createElement('button');
+const newGrid = document.createElement('button');
 const header = document.querySelector('h1');
 
 body.classList.add('body');
@@ -11,7 +12,47 @@ grid.classList.add('grid');
 sidebar.classList.add('sidebar'); 
 clear.classList.add('clear');
 clear.textContent = 'Clear';
+newGrid.classList.add('newGrid');
+newGrid.textContent = 'New';
 
+initialGrid();
+sidebar.appendChild(clear);
+sidebar.appendChild(newGrid);
+body.appendChild(sidebar);
+body.appendChild(grid);
+container.appendChild(body);
+
+clear.addEventListener('click', () => { //Clears the grid
+    clearGrid();
+})
+
+newGrid.addEventListener('click', () => {
+    initialGrid();
+    colorGrid();
+})
+
+function colorGrid () {
+    const gridItems = document.querySelectorAll('.box');
+    for (let gridItem of gridItems) {
+        gridItem.addEventListener('mouseover', function () { //Colors the grid items on mouseover
+            gridItem.style.background = 'purple';
+        }
+    )}
+}
+
+function clearGrid() {
+    const gridItems = document.querySelectorAll('.box');
+    gridItems.forEach(gridItem => {
+        gridItem.style.background = 'white';
+    })
+}
+
+const gridItems = document.querySelectorAll('.box');
+for (let gridItem of gridItems) {
+    gridItem.addEventListener('mouseover', function () { //Colors the grid items on mouseover
+        gridItem.style.background = 'purple';
+    }
+)}
 
 function initialGrid() { //Creates the initial grid with the size based on user input
     numBoxes = parseInt(prompt ("Enter grid width in # of boxes (cannot exceed 64)"));
@@ -24,31 +65,10 @@ function initialGrid() { //Creates the initial grid with the size based on user 
     for (i=0; i<totalBoxes; i++) { 
         createDivs(); 
     }
-    
 }
 
 function createDivs() { //Creates grid items based on user input
     let div = document.createElement('div');
     grid.appendChild(div);
     div.classList.add('box');
-    
 }
-
-initialGrid();
-sidebar.appendChild(clear);
-body.appendChild(sidebar);
-body.appendChild(grid);
-container.appendChild(body);
-
-const gridItems = document.querySelectorAll('.box');
-for (let gridItem of gridItems) {
-    gridItem.addEventListener('mouseover', function () { //Colors the grid items on mouseover
-        gridItem.style.background = 'purple';
-    }
-)}
-
-clear.addEventListener('click', () => { //Clears the grid
-    gridItems.forEach(gridItem => {
-        gridItem.style.background = 'white';
-    })
-})
